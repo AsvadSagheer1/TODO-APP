43c04f40b6981edc1fccfa30d1ce7060a0248082
AsvadSagheer1 <92682536+AsvadSagheer1@users.noreply.github.com>
2023-09-28 22:56:07 +0500

    Full to do app

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-let input = document.querySelector(input)
-
+let input = document.querySelector("input");
+let list = document.querySelector(".tasks-list");
+let task = document.querySelector(".clear-task")
 function addToDo() {
-    console.log("Added")
-    
+    if (input.value !== "") {
+        let newTask = document.createElement("div");
+        newTask.classList.add("d-flex")
+        newTask.classList.add("justify-content-center")
+        newTask.classList.add("my-3")
+        newTask.innerHTML = `<li><p class= "mx-5">${input.value}</p></li> <button class = "clear-task btn btn-danger" onclick = clearThisToDo(this)><i class="fa fa-trash-o"></i></button>`;
+        list.appendChild(newTask);
+        input.value = ""
+    }
+    else {
+alert("Please type something in the box!")
+    }
 }
+
+function clearThisToDo(i) {
+    console.log(i.parentNode.remove())
+}
+
 function clearAll() {
-    console.log("Removed")
+    list.innerHTML = ""
+    input.value = ""
 }

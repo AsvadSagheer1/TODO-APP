dab2248c44080d9df6ade4108b7347a653a4129e
AsvadSagheer1 <92682536+AsvadSagheer1@users.noreply.github.com>
2023-10-01 12:07:15 +0500

    Added edit functionality

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function addToDo() {
         newTask.classList.add("d-flex")
         newTask.classList.add("justify-content-center")
         newTask.classList.add("my-3")
-        newTask.innerHTML = `<li><p class= "mx-5" style = "width : 500px; overflow: hidden">${input.value}</p></li> <button class = "clear-task btn btn-danger" onclick = clearThisToDo(this)><i class="fa fa-trash-o"></i></button>`;
+        newTask.innerHTML = `<li><p style = "width : 500px; display: inline-block">${input.value}</p><button class = "btn btn-success" onClick = editToDo(this)><i class="fa fa-pencil-square-o"></i></button> <button class = "clear-task btn btn-danger" onclick = clearThisToDo(this)><i class="fa fa-trash-o"></i></button></li>`;
         list.appendChild(newTask);
         input.value = ""
         alertBox.style.display = "none";
@@ -26,4 +26,22 @@ function clearAll() {
     list.innerHTML = ""
     input.value = ""
     alertBox.style.display = "none";
+}
+function editToDo(item) {
+    if (item.innerHTML === "Done") {
+        const toDoName1 = item.previousElementSibling.value;
+        const par = document.createElement("p")
+        par.setAttribute("style", "display: inline-block; width : 500px")
+        par.innerHTML = toDoName1;
+        item.parentElement.replaceChild(par, item.previousElementSibling)
+        item.innerHTML = `<i class="fa fa-pencil-square-o"></i>`
+    } else {
+        const toDoName = item.previousElementSibling.textContent;
+        item.textContent = "Done";
+        const input = document.createElement("input");
+        input.value = toDoName;
+        input.type = "text";
+        input.classList.add("mx-5")
+        item.parentElement.replaceChild(input, item.previousElementSibling)
+    }
 }
